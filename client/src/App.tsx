2409f76dc1c8240d4cc08384c2bd6b607d554777
shubhivermax <shubhiverma@usf.edu>
2025-06-23 00:20:28 -0400

    created client server for backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,14 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 import SignIn from './components/magicui/SignIn';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [screen, setScreen] = useState<'signin' | 'signup' | 'landing'>('signin');
 
   return (
+
     <div className="page-container">
       <AuroraText className="aurora-text-massive" speed={2}>GitMatchify</AuroraText>
-      <div className='page-subtitle'>The all-in-one platform to connect</div>
+      <div className='page-subtitle'>Connect with developers for your projects!</div>
+      
       <SignIn />
       <button className='signup'>Sign Up Here</button>
     </div>
