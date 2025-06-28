1b5209f7b40df478ee32c4a0300465d41619d451
shubhivermax <shubhiverma@usf.edu>
2025-06-27 20:48:59 -0400

    login register pages front-end set up

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,27 @@
 
+import { useState
 
+ } from "react"
 const Login = () => {
+
+  const [data,setData]= useState({
+    email:'',
+    password:'',
+  })
+  const loginUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault
+  }
   return (
-    <div>Login</div>
+    <div>Login
+      <form onSubmit={loginUser}>
+         
+         <label>Email</label>
+         <input type='email' placeholder='enter email...' value={data.email} onChange={(e)=>setData({...data,email:e.target.value})}></input>
+         <label>Password</label>
+         <input type='password' placeholder='enter password...' value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}></input>
+         <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,8 +1,33 @@
+import { useState, useEffect} from "react"
 
 
 const Register = () => {
+
+  const [data, setData] = useState({
+    name:'',
+    email:'',
+    password: '',
+
+  })
+  useEffect(() => {
+    console.log("Current form data:", data);
+  }, [data]);
+
+  const registerUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
   return (
-    <div>Register</div>
+    <div>Register
+      <form onSubmit={registerUser}>
+         <label>Name</label>
+         <input type='text' placeholder='enter name...' value={data.name} onChange={(e)=>setData({...data,name:e.target.value})}></input>
+         <label>Email</label>
+         <input type='email' placeholder='enter email...' value={data.email} onChange={(e)=>setData({...data,email:e.target.value})}></input>
+         <label>Password</label>
+         <input type='password' placeholder='enter password...' value={data.password} onChange={(e)=>setData({...data,password:e.target.value})}></input>
+         <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
