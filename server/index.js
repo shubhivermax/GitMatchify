d9a781a4bb8ba2be138c07c0771d8f3d703c62b1
shubhivermax <shubhiverma@usf.edu>
2025-07-20 23:38:17 -0400

    Database connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+
+
 const express = require('express');
 const dotenv = require('dotenv');
+dotenv.config();
 const cors= require('cors');
+const mongoose = require('mongoose');
+
+console.log('Connecting to mongoDB with:', process.env.MONGO_URL);
+//database connection 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.log('not connected', err));
 
 
 const app = express();
